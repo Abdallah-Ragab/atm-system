1546119981a3e65e3d80ed4b6960f860f9a1a08c
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 21:38:22 +0200

    Refactor account.py to add validation for balance, card, PIN, and account number

--- account.py
+++ account.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Optional
 from typing_extensions import Annotated
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 from enum import Enum
 
 
@@ -11,16 +11,17 @@ class TransactionType(str, Enum):
     Transfer = "Transfer"
     Inquiry = "Inquiry"
 
+
 class Transaction(BaseModel):
     type: TransactionType
-    amount: Annotated[Optional[int], Field(default=None, validate_default=True)]
+    amount: Optional[int] = Field(default=None, validate_default=True)
     account_number: Optional[int] = Field(default=None, validate_default=True)
     timestamp: datetime.datetime = datetime.datetime.now()
 
     @field_validator("amount")
     @classmethod
     def validate_amount(cls, v, values, **kwargs):
-        if values.data['type'] == TransactionType.Inquiry:
+        if values.data["type"] == TransactionType.Inquiry:
             return v
         if v is None or v <= 0:
             raise ValueError("amount must be provided and greater than 0")
@@ -29,7 +30,7 @@ class Transaction(BaseModel):
     @field_validator("account_number")
     @classmethod
     def validate_account_number(cls, v, values, **kwargs):
-        if values.data['type'] != TransactionType.Transfer:
+        if values.data["type"] != TransactionType.Transfer:
             return v
         if v is None:
             raise ValueError("account number must be provided for transfer")
@@ -44,10 +45,46 @@ class Transaction(BaseModel):
             _str += f"Account Number: {self.account_number}\n"
         return _str
 
+
 class Account(BaseModel):
     name: str
-    balance: int
-    card: int = None
-    PIN: str = None
-    account_number: int = 0
-    transactions: list[Transaction] = []
+    balance: Optional[int] = 0
+    account_number: int
+    bank: str
+    card: Optional[int] = None
+    PIN: Optional[str] = None
+    transactions: Optional[list[Transaction]] = []
+
+    @field_validator("balance")
+    @classmethod
+    def validate_balance(cls, v, values, **kwargs):
+        print("Validating balance")
+        if v < 0:
+            raise ValueError("balance must be greater or equal to 0")
+        return v
+
+    @field_validator("card")
+    @classmethod
+    def validate_card(cls, v, values, **kwargs):
+        print("Validating card")
+        if v is None:
+            return v
+        if len(str(v)) != 16:
+            raise ValueError("card number must be 16 digits")
+        return v
+
+    @model_validator(mode="after")
+    @classmethod
+    def validate_card_and_PIN(cls, values, **kwargs):
+        print("Validating card and PIN")
+        if values.card is not None and values.PIN is None:
+            raise ValueError("PIN must be provided with card")
+        return values
+
+    @field_validator("account_number")
+    @classmethod
+    def validate_account_number(cls, v, values, **kwargs):
+        print("Validating account number")
+        if len(str(v)) != 14:
+            raise ValueError("account number must be 14 digits")
+        return v
