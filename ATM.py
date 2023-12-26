f23d2c0d7efc65165855a0fde228f92dd3ec2bbe
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 22:34:39 +0200

    Fix bug in login functionality

--- ATM.py
+++ ATM.py
@@ -1,104 +1,119 @@
 import datetime
+from storage import JSONStorage
+from account import Account, Transaction, TransactionType, TransactionStatus
+
 
 class CODE:
     def __init__(self, code, message):
         self.code = code
         self.message = message
 
+    def __str__(self):
+        if self.code.startswith("1"):
+            return f"INFO({self.code}): {self.message}"
+        elif self.code.startswith("0"):
+            return f"ERROR({self.code}): {self.message}"
+        else:
+            return f"UNKNOWN({self.code}): {self.message}"
+    def __repr__(self):
+        return self.__str__()
+
+
 class Bank:
+    SUCCESS = CODE("1-000", "Transaction successful.")
+    VALID_ACCOUNT = CODE("1-001", "This account is valid.")
+    VALID_ACCOUNT_INFO = CODE("1-001", "The info provided are vaild.")
+    ACCOUNT_EXISTS = CODE("1-002", "This account exists.")
 
-    SUCCESS = CODE('1-000', "Transaction successful.")
-    VALID_ACCOUNT = CODE('1-001', "This account is valid.")
-    VALID_ACCOUNT_INFO = CODE('1-001', "The info provided are vaild.")
-    ACCOUNT_EXISTS = CODE('1-002', "This account exists.")
+    FAILED = CODE("0-000", "Transaction failed.")
+    NO_ACCOUNT = CODE("0-001", "This account does not exist.")
+    INSUFFICIENT_BALANCE = CODE("0-002", "Insufficient balance. Please try again.")
+    INVALID_AMOUNT = CODE("0-003", "Invalid amount. Please try again.")
+    WRONG_PIN = CODE("0-004", "Wrong PIN. Please try again.")
+    INVALID_CARD = CODE("0-005", "Invalid card number. Please try again.")
+    DUPLICATE_ACCOUNT = CODE("0-007", "This account already exists.")
 
-
-    FAILED = CODE('0-000', "Transaction failed.")
-    NO_ACCOUNT = CODE('0-001', "This account does not exist.")
-    INSUFFICIENT_BALANCE = CODE('0-002', "Insufficient balance. Please try again.")
-    INVALID_AMOUNT = CODE('0-003', "Invalid amount. Please try again.")
-    WRONG_PIN = CODE('0-004', "Wrong PIN. Please try again.")
-    INVALID_CARD = CODE('0-005', "Invalid card number. Please try again.")
-    DIFFERENT_BANK = CODE('0-006', "This card is not issued by this bank.")
-    DUPLICATE_ACCOUNT = CODE('0-007', "This account already exists.")
-
-
-
-    def __init__(self, name, prefix, accounts):
-        self.name = name
-        self.prefix = prefix
-        self.accounts = accounts
-
-    def account_exists(self, account):
-        for acc in self.accounts:
-            if acc.card == account.card:
-                return Bank.ACCOUNT_EXISTS
+    @staticmethod
+    def print_receipt(account_number):
+        account = Bank._get_account(account_number)
+        if isinstance(account, Account):
+            transaction = account.transactions[-1]
+            print(transaction.generate_receipt(account))
+            return Bank.SUCCESS
         return Bank.NO_ACCOUNT
 
-    def validate_account_info(self, account):
-        if not account.card.startswith(self.prefix):
-            return Bank.DIFFERENT_BANK
-        if len(account.card) != 16:
-            return Bank.INVALID_CARD
-        return Bank.VALID_ACCOUNT_INFO
+    @staticmethod
+    def withdraw(account_number, amount):
+        transaction_type = TransactionType.Withdraw
+        amount = int(amount)
 
-    def validate_account(self, account):
-        account_exists = self.account_exists(account)
-        if account_exists != Bank.ACCOUNT_EXISTS:
-            return account_exists
-        account_info = self.validate_account_info(account)
-        if account_info != Bank.VALID_ACCOUNT_INFO:
-            return account_info
-        return Bank.VALID_ACCOUNT
+        account = Bank._get_account(account_number)
 
-    def add_account(self, account):
-        account_exists = self.account_exists(account)
-        if account_exists == Bank.ACCOUNT_EXISTS:
-            return False, account_exists.message
+        if isinstance(account, Account):
+            last_transaction_id = Bank._last_transaction_id(account)
+            transaction = Transaction(
+                id=last_transaction_id + 1, type=transaction_type, amount=amount
+            )
+            if account.balance >= amount:
+                account.balance -= amount
+                transaction.update_status(TransactionStatus.SUCCESS)
+                account.transactions.append(transaction)
+                JSONStorage.save_account(account.model_dump())
+                return Bank.SUCCESS
+            transaction.update_status(Transaction.FAILED)
+            account.transactions.append(transaction)
+            JSONStorage.save_account(account.model_dump())
+            return Bank.INSUFFICIENT_BALANCE
+        else:
+            return Bank.NO_ACCOUNT
 
-        info = self.validate_account_info(account)
+    def deposit(account, amount):
+        pass
 
-        if info == self.VALID_ACCOUNT:
-            return info
+    def balance_inquiry(account):
+        pass
 
+    def transfer(account, amount, target_account):
+        pass
 
-    def remove_account(self, account):
-        code = self.validate_account(account)
-        if code == Bank.VALID_ACCOUNT:
-            self.accounts.remove(account)
-            return True, code.message
-        return False, code.message
+    def change_PIN(account, new_PIN):
+        pass
 
-    def get_account(self, card, PIN):
-        for account in self.accounts:
-            if account.validate(card, PIN):
-                return account, Bank.SUCCESS.message
-        return False, Bank.NO_ACCOUNT.message
+    def history(account):
+        pass
 
-    def withdraw(self, account, amount):
-        code = self.validate_account(account)
-        if not code == Bank.VALID_ACCOUNT:
-            return False, code.message
-        if account.withdraw(amount):
-            return True, Bank.SUCCESS.message
-        return False, Bank.INSUFFICIENT_BALANCE.message
+    @staticmethod
+    def _save_transaction(transaction, account):
+        account.transactions.append(transaction)
+        JSONStorage.save_account(account.model_dump())
 
-    def deposit(self, account, amount):
-        code = self.validate_account(account)
-        if not code == Bank.VALID_ACCOUNT:
-            return False, code.message
-        account.deposit(amount)
-        return True, Bank.SUCCESS.message
+    @staticmethod
+    def _last_transaction_id(account):
+        try:
+            return account.transactions[-1].id
+        except IndexError:
+            return 0
 
-    def change_PIN(self, account, new_PIN):
-        account.change_PIN(new_PIN)
-        return True
+    @staticmethod
+    def _account_exists(account: Account):
+        account = JSONStorage.get_account(account.number)
+        if account:
+            return Bank.ACCOUNT_EXISTS
+        return Bank.NO_ACCOUNT
 
-    def transfer(self, account, amount, target_account):
-        if self.withdraw(account, amount):
-            self.deposit(target_account, amount)
-            return True
-        return False
+    @staticmethod
+    def _get_account(account_number):
+        account = JSONStorage.get_account(account_number)
+        if account:
+            try:
+                account = Account(**account)
+                return account
+            except ValueError as e:
+                print(e)
+                return Bank.INVALID_ACCOUNT
+        else:
+            return Bank.NO_ACCOUNT
+
 
 class SAFE:
     DEFAULT_NOTES = {20: 100, 50: 100, 100: 100, 200: 100}
@@ -125,53 +140,54 @@ class SAFE:
         for note, count in SAFE.NOTES.items():
             print(f"{note} EGP notes: {count}")
 
-class Account:
-    def __init__(self, name, balance, card, PIN):
-        self.name = name
-        self.balance = balance
-        self.card = card
-        self.PIN = PIN
 
-    def deposit(self, amount):
-        self.balance += amount
+# class Account:
+#     def __init__(self, name, balance, card, PIN):
+#         self.name = name
+#         self.balance = balance
+#         self.card = card
+#         self.PIN = PIN
 
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-            return True
-        return False
+#     def deposit(self, amount):
+#         self.balance += amount
 
-    def change_PIN(self, new_PIN):
-        self.PIN = new_PIN
+#     def withdraw(self, amount):
+#         if self.balance >= amount:
+#             self.balance -= amount
+#             return True
+#         return False
 
-    def validate(self, card, PIN):
-        if self.card == card and self.PIN == PIN:
-            return True
-        return False
+#     def change_PIN(self, new_PIN):
+#         self.PIN = new_PIN
 
-
-class Printer:
-    @staticmethod
-    def print_receipt(account, amount, transaction_type):
-        print(f"Account Name: {account.name}")
-        print(f"Account Balance: {account.balance}")
-        print(f"Transaction Type: {transaction_type}")
-        print(f"Transaction Amount: {amount}")
-        print(f"New Balance: {account.balance}")
-        print(f"Time: {datetime.now()}")
+#     def validate(self, card, PIN):
+#         if self.card == card and self.PIN == PIN:
+#             return True
+#         return False
 
 
-class Transaction:
-    types = ["Withdraw", "Deposit"]
-    statuses = ["Success", "Failed", "Cancelled", "Pending"]
-    def __init__(self, account, amount, transaction_type, status = "Pending"):
-        self.account = account
-        self.amount = amount
-        self.transaction_type = transaction_type
-        self.transaction_time = datetime.now()
-        self.status = status
+# class Printer:
+#     @staticmethod
+#     def print_receipt(account, amount, transaction_type):
+#         print(f"Account Name: {account.name}")
+#         print(f"Account Balance: {account.balance}")
+#         print(f"Transaction Type: {transaction_type}")
+#         print(f"Transaction Amount: {amount}")
+#         print(f"New Balance: {account.balance}")
+#         print(f"Time: {datetime.now()}")
 
 
+# class Transaction:
+#     types = ["Withdraw", "Deposit"]
+#     statuses = ["Success", "Failed", "Cancelled", "Pending"]
 
-class ATM:
-    banks = {}
+#     def __init__(self, account, amount, transaction_type, status="Pending"):
+#         self.account = account
+#         self.amount = amount
+#         self.transaction_type = transaction_type
+#         self.transaction_time = datetime.now()
+#         self.status = status
+
+
+# class ATM:
+#     banks = {}
